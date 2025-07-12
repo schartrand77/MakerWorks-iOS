34a7ccb35927f46563851892db1d30bbc8a36f69
techpunk <60296853+schartrand77@users.noreply.github.com>
2025-07-12 12:18:40 -0400

    Add upload model endpoint and use network client

---
+++ Sources/Tests/MakerWorksTests/UploadEndpointTests.swift
@@ -0,0 +1,16 @@
+import XCTest
+@testable import MakerWorks
+
+final class UploadEndpointTests: XCTestCase {
+    func testUploadRequest() throws {
+        let baseURL = URL(string: "https://api.makerworks.app")!
+        let data = "hello".data(using: .utf8)!
+        let boundary = "XYZ"
+        let request = APIEndpoint.uploadModel(data: data, boundary: boundary).urlRequest(baseURL: baseURL)
+
+        XCTAssertEqual(request.httpMethod, "POST")
+        XCTAssertEqual(request.url?.absoluteString, "https://api.makerworks.app/api/v1/upload/")
+        XCTAssertEqual(request.value(forHTTPHeaderField: "Content-Type"), "multipart/form-data; boundary=\(boundary)")
+        XCTAssertEqual(request.httpBody, data)
+    }
+}
